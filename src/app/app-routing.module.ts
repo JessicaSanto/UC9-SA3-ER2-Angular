91e1e17ceeeb38700ede13fdd4ace1bca7a5f358
JessicaSanto <jessicaesanto@outlook.com>
2022-02-24 14:55:31 -0300

    Angula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"contato", component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HeaderComponent } from './views/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
